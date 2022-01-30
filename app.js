563b88cb385eb33f53ffd8612f9230bf6d9e93c0
RamjithRamadas <ramjithramadas@gmail.com>
2022-01-30 14:05:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// to serve statically -->
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+   res.status(404).render("404", { pageTitle: "Page Not Found!", path: "Error" });
+});
+
+app.listen(3001, () => {
+   console.log("Server is running on port 3001");
+});
---
+++ routes/shop.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const adminData = require("./admin");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+   console.log("shop.js", adminData.products);
+   const products = adminData.products;
+
+   res.render("shop", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true,
+   });
+});
+
+module.exports = router;
