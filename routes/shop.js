6c5da1b5319248686c1859ae73361e907b7106be
RamjithRamadas <ramjithramadas@gmail.com>
2022-01-30 14:19:56 +0530

    added product controller

--- routes/shop.js
+++ routes/shop.js
@@ -1,21 +1,9 @@
 const express = require("express");
 
-const adminData = require("./admin");
+const productController = require("../controllers/product");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-   console.log("shop.js", adminData.products);
-   const products = adminData.products;
-
-   res.render("shop", {
-      prods: products,
-      pageTitle: "Shop",
-      path: "/",
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true,
-   });
-});
+router.get("/", productController.getAllProducts);
 
 module.exports = router;
