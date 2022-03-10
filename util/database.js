48d5d9756ea121b19b97003eb34b8b2fbcd41af7
RamjithRamadas <ramjithramadas@gmail.com>
2022-03-10 12:22:55 +0530

    without sequelize

--- util/database.js
+++ util/database.js
@@ -8,3 +8,12 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool.promise();
+
+// const Sequelize = require("sequelize");
+
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//    host: process.env.DB_HOST,
+//    dialect: "mysql",
+// });
+
+// module.exports = sequelize;
