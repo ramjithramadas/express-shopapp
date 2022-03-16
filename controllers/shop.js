aee7649b3ab81008b5a65c46f7954e5bfffa65ac
ramjith <ramjith@revyrieglobal.com>
2022-03-16 18:38:36 +0530

    order func added

--- controllers/shop.js
+++ controllers/shop.js
@@ -75,50 +75,28 @@ exports.postCart = (req, res, next) => {
         .catch((err) => console.error(err));
 };
 
-// exports.getOrders = (req, res, next) => {
-//    req.user
-//       .getOrders({ include: ["products"] })
-//       .then((orders) => {
-//          res.render("shop/orders", {
-//             path: "/orders",
-//             pageTitle: "Your Orders",
-//             orders: orders,
-//          });
-//       })
-//       .catch((err) => console.log(err));
-// };
+exports.getOrders = (req, res, next) => {
+    req.user
+        .getOrders()
+        .then((orders) => {
+            res.render("shop/orders", {
+                path: "/orders",
+                pageTitle: "Your Orders",
+                orders: orders,
+            });
+        })
+        .catch((err) => console.log(err));
+};
 
-// exports.postOrder = (req, res, next) => {
-//    let fetchedCart;
-//    req.user
-//       .getCart()
-//       .then((cart) => {
-//          fetchedCart = cart;
-//          //  console.log(cart, "cartcart");
-//          return cart.getProducts();
-//       })
-//       .then((products) => {
-//          // console.log(products, "productsproducts");
-//          return req.user
-//             .createOrder()
-//             .then((order) => {
-//                return order.addProducts(
-//                   products.map((product) => {
-//                      product.orderItem = { quantity: product.cartItem.quantity };
-//                      return product;
-//                   })
-//                );
-//             })
-//             .catch((err) => console.log(err));
-//       })
-//       .then(() => {
-//          return fetchedCart.setProducts(null);
-//       })
-//       .then(() => {
-//          res.redirect("/orders");
-//       })
-//       .catch((err) => console.log(err));
-// };
+exports.postOrder = (req, res, next) => {
+    let fetchedCart;
+    req.user
+        .addOrder()
+        .then(() => {
+            res.redirect("/orders");
+        })
+        .catch((err) => console.log(err));
+};
 
 // exports.getCheckout = (req, res, next) => {
 //    res.render("shop/checkout", {
--- models/user.js
+++ models/user.js
@@ -97,6 +97,39 @@ class User {
             })
             .catch((err) => console.log(err));
     }
+
+    addOrder() {
+        const db = getDb();
+        return this.getCartItems()
+            .then((products) => {
+                const order = {
+                    items: products,
+                    user: {
+                        _id: new ObjectId(this._id),
+                        name: this.username,
+                    },
+                };
+                return db.collection("orders").insertOne(order);
+            })
+            .then((result) => {
+                this.cart = { item: [] };
+                return db
+                    .collection("users")
+                    .updateOne(
+                        { _id: new ObjectId(this._id) },
+                        { $set: { cart: { items: [] } } }
+                    );
+            })
+            .catch((err) => console.log(err));
+    }
+
+    getOrders() {
+        const db = getDb();
+        return db
+            .collection("orders")
+            .find({ "user._id": new ObjectId(this._id) })
+            .toArray();
+    }
 }
 
 module.exports = User;
